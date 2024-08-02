c85c9445bdd5ad86cbb2273276edba8fb6647fc2
mangal-bodele <163996259+mangal-bodele@users.noreply.github.com>
2024-08-02 15:43:19 +0530

    Update models.py

--- client_management/app1/models.py
+++ client_management/app1/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
 class Employee(models.Model):
-    PROJECTDOMAIN = [ 'SAAS','Ecommerce', 'CRM', 'ERP', 'Finance']
-    STATUS = ['Onboarded' , 'Not Onboarded']
+    PROJECTDOMAIN = [
+        ('SAAS', 'SAAS'),
+        ('Ecommerce', 'Ecommerce'),
+        ('CRM', 'CRM'),
+        ('ERP', 'ERP'),
+        ('Finance', 'Finance')
+    ]
+    STATUS = [
+        ('Onboarded', 'Onboarded'),
+        ('Not Onboarded', 'Not Onboarded')
+    ]
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     company = models.CharField(max_length=50)
     email_id = models.CharField(max_length=100, unique=True)
     department = models.CharField(max_length=50)
     dateOfJoining = models.DateField()
-    status = models.CharField(max_length=50,choices=STATUS)
-    projectDomain = models.CharField(max_length=50,choices=PROJECTDOMAIN)
+    status = models.CharField(max_length=50, choices=STATUS)
+    projectDomain = models.CharField(max_length=50, choices=PROJECTDOMAIN)
 
-    def __str__(self):
+    def _str_(self):
         return self.name
